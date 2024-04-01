c177ccef4da1a2fb06c31748e1f70c9dd076a1c9
Nathan Peters <peters.nate8@gmail.com>
2024-04-01 16:01:55 -0600

    create action to create new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,29 @@
 class UsersController < ApplicationController
+    # POST /users
+    def create
+        # Create a new user
+        @user = User.new(user_params)
+
+        # If the user is saved successfully, return the user
+        if @user.save
+            render json: { 
+                user: { 
+                    username: @user.username, 
+                    email: @user.email
+                }
+            } 
+        # If the user is not saved successfully, return success: false
+        else
+            render json: {
+                success: false,
+            }
+        end
+    end
+
+    # handle user parameters
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
